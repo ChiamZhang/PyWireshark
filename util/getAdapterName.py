33fa8cbc39f5639c55131e20748e53289a789e5b
Chiam <chiam_zhang@foxmail.com>
2023-03-21 22:13:24 +0800

    获取网卡名称，用于主页面的网卡选择

---
+++ util/getAdapterName.py
@@ -0,0 +1,20 @@
+
+from scapy.all import *
+
+
+def get_adapter_name():
+
+    adapterName=[]
+    adapterInfor=show_interfaces(False)
+    tem=adapterInfor.split('\n')
+    for name in tem:
+        a= name.strip().split('     ')
+        if len(a)>3:
+            adapterName.append(a[1])
+    del(adapterName[0])
+    # print(adapterName)
+    return adapterName
+
+def get_adapter_information():
+    return show_interfaces(False)
+
